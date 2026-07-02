94caf10f51d8e286d11824ea8ee36be20a569fab
naruto22201-dev <narutoengloo@gmail.com>
2026-07-02 20:05:09 +0200

    Initial: Add Color.kt

---
+++ app/src/main/kotlin/com/naruto22201/minecraftjava/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.naruto22201.minecraftjava.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark theme colors
+val md_theme_dark_primary = Color(0xFF90CAF9)
+val md_theme_dark_secondary = Color(0xFF81C784)
+val md_theme_dark_tertiary = Color(0xFFFFB74D)
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_surface = Color(0xFF1E1E1E)
+
+// Light theme colors
+val md_theme_light_primary = Color(0xFF1976D2)
+val md_theme_light_secondary = Color(0xFF388E3C)
+val md_theme_light_tertiary = Color(0xFFF57C00)
+val md_theme_light_background = Color(0xFFFAFAFA)
+val md_theme_light_surface = Color(0xFFFFFFFF)
+
+// Minecraft colors
+val MinecraftGreen = Color(0xFF55AA00)
+val MinecraftDarkGreen = Color(0xFF005500)
+val MinecraftRed = Color(0xFFCC0000)
